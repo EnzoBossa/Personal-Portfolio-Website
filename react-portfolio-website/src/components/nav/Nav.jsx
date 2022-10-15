119fbb4ec20193d917a6675207ab02b7ab89df27
Enzo Bossa <89361489+EnzoBossa@users.noreply.github.com>
2022-10-15 15:03:57 -0400

    nav update
    Working on the nav component

--- react-portfolio-website/src/components/nav/Nav.jsx
+++ react-portfolio-website/src/components/nav/Nav.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './nav.css'
-
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBook, BiMessageSquareDetail} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+import {useState} from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#' onClick={() => setActiveNav('#')} className={setActiveNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
+      <a href='#about' onClick={() => setActiveNav('#about')} className={setActiveNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
+      <a href='#experience' onClick={() => setActiveNav('#experience')} className={setActiveNav === '#experience' ? 'active' : ''}><BiBook/></a>
+      <a href='#services'onClick={() => setActiveNav('#services')} className={setActiveNav === '#services' ? 'active' : ''}><RiServiceLine/></a>
+      <a href='#contact' onClick={() => setActiveNav('#contact')} className={setActiveNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail/></a>
+    </nav>
   )
 }
 
