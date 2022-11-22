aa073f2af048b7df5387ef9178de718e70b74f32
Enzo Bossa <ebossa2018@fau.edu>
2022-11-22 00:33:11 -0500

    about, experience, services components done
    Completed the about, experience, and services components.

--- react-portfolio-website/src/components/about/About.jsx
+++ react-portfolio-website/src/components/about/About.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.JPG'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className='container about__container'>
+        <div className='about__me'>
+          <div className="about__me-image">
+            <img src={ME} alt='About Image' />
+          </div>
+        </div>
+
+        <div className='about__content'>
+          <div className="about__cards">
+
+            <article className='about__card'>
+              <FaAward  className='about__icon'/>
+              <h5>Experience</h5>
+              <small>2+ Years Working on Personal Projects</small>
+            </article>
+
+            <article className='about__card'>
+              <VscFolderLibrary  className='about__icon'/>
+              <h5>Projects</h5>
+              <small>2 Academic Projects</small>
+            </article>
+          </div>
+
+          <p>
+            I have experience working with React and Firebase. I also have experience with Augmented Reality
+            and Unity development as well as developing on the HoloLens 2 device. 
+          </p>
+
+          <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- react-portfolio-website/src/components/services/Services.jsx
+++ react-portfolio-website/src/components/services/Services.jsx
@@ -1,10 +1,62 @@
 import React from 'react'
 import './services.css'
-
+import {BiCheck} from 'react-icons/bi'
 
 const Services = () => {
   return (
-    <section id='services'>Services</section>
+    <section id='services'>
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className='service'>
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Front-end design and development</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Back-end development</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>API integration</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Responsive website</p>
+            </li>
+          </ul>
+        </article>
+        <article className='service'>
+          <div className="service__head">
+            <h3>Video Editing</h3>
+          </div>
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Trimming and merging clips</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>YouTube video editing</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Gaming videos and montages</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Custom video editing per request</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
   )
 }
 
