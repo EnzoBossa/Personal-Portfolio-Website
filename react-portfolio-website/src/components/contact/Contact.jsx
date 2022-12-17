bb6570253c8b106c36e94493a8f06666288226d7
Enzo Bossa <89361489+EnzoBossa@users.noreply.github.com>
2022-12-17 14:45:28 -0500

    Updated contact.jsx
    skeleton for the contact section

--- react-portfolio-website/src/components/contact/Contact.jsx
+++ react-portfolio-website/src/components/contact/Contact.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {AiOutlinePhone} from 'react-icons/ai'
+import {AiOutlineLinkedin} from 'react-icons/ai'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container container__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail/>
+            <h4>Email</h4>
+            <h5>enzobossa12@gmail.com</h5>
+            <a href="mailto:enzobossa12@gmail.com">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <AiOutlinePhone/>
+            <h4>Phone</h4>
+            <h5>+1(786)-563-6706</h5>
+            <a href="tel:7865636706">Call or send a message</a>
+          </article>
+          <article className="contact__option">
+            <AiOutlineLinkedin/>
+            <h4>LinkedIn</h4>
+            <h5>Enzo Bossa</h5>
+            <a href="https://www.linkedin.com/in/enzo-bossa/" target="_blank" rel='noreferrer'>Send a message</a>
+          </article>
+        </div>
+        {/*END OF CONTACT OPTIONS*/} 
+        <form action="">
+          <input type="text" name="name" placeholder="Your Full Name" required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required ></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
