48e147c8ec10cedfc19007f71d8e38e78ab3bf42
Enzo Bossa <ebossa2018@fau.edu>
2022-12-21 01:11:32 -0500

    Finished website
    finished the contact section with css. Did the footer section. Built and deployed the website at enzobossa.com

--- react-portfolio-website/src/components/contact/Contact.jsx
+++ react-portfolio-website/src/components/contact/Contact.jsx
@@ -3,8 +3,24 @@ import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
 import {AiOutlinePhone} from 'react-icons/ai'
 import {AiOutlineLinkedin} from 'react-icons/ai'
+import {useRef} from 'react';
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_hi2e1sa', 'template_wfm8t4q', form.current, 'l9G5KSZ333XdKa1l_')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+      e.target.reset()
+  };
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -13,26 +29,26 @@ const Contact = () => {
       <div className="container container__container">
         <div className="contact__options">
           <article className="contact__option">
-            <MdOutlineEmail/>
+            <MdOutlineEmail className='contact__option-icon'/>
             <h4>Email</h4>
             <h5>enzobossa12@gmail.com</h5>
             <a href="mailto:enzobossa12@gmail.com">Send a message</a>
           </article>
           <article className="contact__option">
-            <AiOutlinePhone/>
+            <AiOutlinePhone className='contact__option-icon'/>
             <h4>Phone</h4>
             <h5>+1(786)-563-6706</h5>
             <a href="tel:7865636706">Call or send a message</a>
           </article>
           <article className="contact__option">
-            <AiOutlineLinkedin/>
+            <AiOutlineLinkedin className='contact__option-icon'/>
             <h4>LinkedIn</h4>
             <h5>Enzo Bossa</h5>
             <a href="https://www.linkedin.com/in/enzo-bossa/" target="_blank" rel='noreferrer'>Send a message</a>
           </article>
         </div>
         {/*END OF CONTACT OPTIONS*/} 
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name="name" placeholder="Your Full Name" required />
           <input type="email" name='email' placeholder='Your Email' required />
           <textarea name="message" rows="7" placeholder='Your Message' required ></textarea>
--- react-portfolio-website/src/components/footer/Footer.jsx
+++ react-portfolio-website/src/components/footer/Footer.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import './footer.css'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
+import {BsStackOverflow} from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>Enzo Bossa</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.linkedin.com/in/enzo-bossa/" target="_blank" rel='noreferrer'><BsLinkedin/></a>
+        <a href="https://github.com/EnzoBossa" target="_blank" rel='noreferrer'><BsGithub/></a>
+        <a href="https://stackoverflow.com/users/18705628/enzo-bossa" target="_blank" rel='noreferrer'><BsStackOverflow/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Enzo Bossa. All rights reserved</small>
+      </div>
+    </footer>
   )
 }
 
